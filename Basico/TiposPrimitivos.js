10aa29107f1deb708257ee3f929e0e2f9a32977c
Carlosouzavalle <Carlosouzavalle@gmail.com>
2025-07-21 21:22:48 -0300

    feat: TiposPrimitivos

---
+++ Basico/TiposPrimitivos.js
@@ -0,0 +1,29 @@
+/*
+    Tipos primitivos em TypeScript
+    - string
+    - number
+    - boolean
+    - null
+    - undefined
+    - symbol
+    - bigint
+    - void (usado em funções que não retornam nada)
+    - any (usado quando não se sabe o tipo da variável)
+    - never (usado quando uma função nunca retorna, por exemplo, uma função que lança um erro)
+*/
+// string
+// let nome2: string = 'Carlos';
+// console.log(nome2);
+// number
+// ele é usado para representar números inteiros e de ponto flutuante
+// let idade2: number = 30;
+// console.log("sua idade é: " + idade2);
+// boolean
+//  ele é usado para representar valores lógicos, verdadeiro ou falso
+var a = 2;
+if (a === 2) {
+    console.log("isso é verdade");
+}
+else {
+    console.log("isso é falso");
+}
